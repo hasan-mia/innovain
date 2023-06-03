a64b9c55a1a2159a652f962eb19316ed2c8c83d0
hasan-mia <hasanrafi69@gmail.com>
2023-06-03 09:52:19 +0600

    setup socket io server

---
+++ config/DbConnection.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-undef */
+require("dotenv").config(); // load .env variables
+const mongoose = require("mongoose"); //import fresh mongoose object
+mongoose.set("strictQuery", false);
+
+const dbUrl = process.env.DATABASE_URL;
+const DbConnection = async () => {
+  try {
+    await mongoose.connect(dbUrl, () => {
+      console.log("Connect to MongoDB");
+    });
+  } catch (error) {
+    console.log(error);
+  }
+  return DbConnection();
+};
+module.exports = DbConnection;
--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 /* eslint-disable no-undef */
 require("dotenv").config();
+const http = require("http");
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
-const connection = require("./config/connection");
+const socketIO = require("socket.io");
+// db connection and route
+const DbConnection = require("./config/DbConnection");
 const authRoute = require("./routes/api/v1/auth.route");
 const userRoute = require("./routes/api/v1/user.route");
 const postRoute = require("./routes/api/v1/post.route");
@@ -20,13 +23,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(express.static("views"));
 
+// create server
+const server = http.createServer(app);
+
 // Port
 const port = process.env.PORT || 5000 || 6000 || 9000;
 
 // ===================================//
 //      Connect TO MONGOODB           //
 //====================================//
-connection();
+DbConnection();
 
 // ===================================//
 //        SERVER / API ROUTES         //
@@ -40,7 +46,25 @@ app.use("/api/v1/post", postRoute); // post route
 // // ===================================//
 // //      setup socket IO Server        //
 // //====================================//
+const io = socketIO(server);
 
-app.listen(port, () => {
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  socket.on("message", (data) => {
+    console.log("Received message:", data);
+    // Handle the received message
+
+    // Emit a custom event to the connected clients
+    io.emit("customEvent", "This is a custom event");
+  });
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+    // Clean up resources or perform other necessary tasks
+  });
+});
+
+server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
